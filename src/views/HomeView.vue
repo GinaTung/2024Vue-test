5a15eca6d8fef700289a748c2ab8f516bcec1840
yuling <gb2013429@gmail.com>
2024-01-25 22:45:05 +0800

    建立vite+vue環境

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <div class="btn btn-primary">1234</div>
+    <TheWelcome />
+  </main>
+</template>
